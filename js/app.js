7a665b3e5d939cc237e9a3ca69a641f5d297de1f
Thai <thainax101@gmail.com>
2021-11-13 21:26:32 +0700

    upload code len git

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slick-slides').slick({
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-arrow-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-arrow-right' aria-hidden='true'></i></button>"
+    });
+  });
+  
