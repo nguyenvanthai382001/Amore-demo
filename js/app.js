e6bfd0467da2b9a1933adc54c754c04db3cb2d22
Thai <thainax101@gmail.com>
2021-11-14 20:36:59 +0700

    update code

--- js/app.js
+++ js/app.js
@@ -7,5 +7,15 @@ $(document).ready(function(){
         prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-arrow-left' aria-hidden='true'></i></button>",
         nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-arrow-right' aria-hidden='true'></i></button>"
     });
+
+    $('.project-list').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-arrow-left' aria-hidden='false'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-arrow-right' aria-hidden='false'></i></button>"
+      });
   });
   
